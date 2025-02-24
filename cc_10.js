767896ea569efd68980d0ea6ac3d180c5c83f602
Julia <lorrayne@usf.edu>
2025-02-23 23:33:16 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -22,6 +22,25 @@ prod1.updateStock(3);
 console.log(prod1.getDetails()); 
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
 
+//Task 2: Creating an Order Class
+class Order {
+    constructor(orderid, product, quantity) {
+        this.orderid = orderid;
+        this.product = product;
+        this.quantity = quantity;
+        this.product.updateStock(this.quantity);
+    };
+    getOrderDetails() {
+        return `Order ID: ${this.orderid}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price*this.quantity}`;   
+    };
+}
+
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
 
 
 
